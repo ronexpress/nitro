cb75b0e356edecccc3bbe509127d0ea9ef7cc2cd
VZL <info@ronexpress.hu>
2021-09-05 19:12:20 +0200

    Create web elements

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,44 @@
+
+function makeAMove(type, xCoordinate, yCoordinate) {
+    $.ajax({
+        url: url + "/game/gameplay",
+        type: 'POST',
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify({
+            "type": type,
+            "coordinateX": xCoordinate,
+            "coordinateY": yCoordinate,
+            "gameId": gameId
+        }),
+        success: function (data) {
+            gameOn = false;
+            displayResponse(data);
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    })
+}
+
+function displayResponse(data) {
+    let board = data.board.board;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            let id = "D"+i+j;
+            $("#" + id).text(board[i][j]);
+        }
+    }
+    if (data.status.hasWinner) {
+        $("#message").text("Winner is " + data.status.winner);
+    }
+    if (data.status.boardFull) {
+        $("#message").text("It is TIE!");
+    }
+    $("#win1").text(data.score.pl1Win);
+    $("#win2").text(data.score.pl2Win);
+    $("#tie").text(data.score.tie);
+    document.getElementById("pl"+data.actualPlayer).style.backgroundColor = "lightblue";
+    document.getElementById("pl"+(1-data.actualPlayer)).style.backgroundColor = "#207020";
+    gameOn = true;
+}
---
+++ src/main/resources/static/js/socket.js
@@ -0,0 +1,88 @@
+const url = 'http://localhost:8080';
+
+function startGame() {
+    let player1 = document.getElementById("player1").value;
+    let player1type = document.getElementById("player1type").value;
+    let player2 = document.getElementById("player2").value;
+    let player2type = document.getElementById("player2type").value;
+
+    $("#message").text("");
+
+    if (player1 == null || player1 === '' || player2 == null || player2 === '') {
+        alert("Please enter name");
+    } else {
+        $.ajax({
+            url: url + "/game/start",
+            type: 'POST',
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify(
+            [
+                {
+                    "name": player1,
+                    "type": player1type
+                },
+                {
+                    "name": player2,
+                    "type": player2type
+                }
+            ]
+            ),
+            success: function (data) {
+                playerType = 'X';
+                document.getElementById("pl0").style.backgroundColor = "lightblue";
+                displayResponse(data);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        })
+    }
+}
+
+
+function ready() {
+    $.ajax({
+        url: url + "/game/status",
+        type: 'GET',
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify(),
+        success: function (data) {
+            if(data.ready && !data.fullBoard){
+                return true;
+            }
+        },
+        error: function (error) {
+            console.log(error);
+            return false;
+        }
+    })
+}
+
+
+function move(place) {
+    if(ready){
+        let x = place / 10;
+        let y = place % 10;
+        //alert("READY "+x+":"+y);
+        $.ajax({
+            url: url + "/game/move",
+            type: 'POST',
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify(
+                [x,y]
+            ),
+            success: function (data) {
+                displayResponse(data);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        })
+    }
+}
+
+
+
